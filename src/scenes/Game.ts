cf5c00dfeacbe035356a774d1ea2aa4d668d249f
NarawitPisaspongsa <6733123921@student.chula.ac.th>
2025-03-30 10:54:25 +0700

    movement

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,6 +2,9 @@ import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene
 {
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+    private chara!: Phaser.Physics.Arcade.Sprite;
+
 	constructor()
 	{
 		super('game')
@@ -9,7 +12,7 @@ export default class Game extends Phaser.Scene
 
 	preload()
     {
-
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     create()
@@ -29,6 +32,62 @@ export default class Game extends Phaser.Scene
             faceColor: new Phaser.Display.Color(255, 0, 0, 255  )
         })
 
-        const charTiles = this.add.sprite(128, 128, 'charTiles', 'charFront.png')
+        this.chara = this.physics.add.sprite(128, 128, 'charTiles', 'charFront.png')
+
+        this.anims.create({
+            key:'charFront',
+            frames: 'charFront.png'
+        })
+
+        this.anims.create({
+            key:'charBack',
+            frames: 'charBack.png'
+        })
+
+        this.anims.create({
+            key:'charRight',
+            frames: 'charRight.png'
+        })
+
+        this.anims.create({
+            key:'charLeft',
+            frames: 'charLeft.png'
+        })
+
+        this.chara.play('charFront')
+    }
+
+    update(time: number, delta: number): void {
+        if(!this.chara || !this.cursors){return;}
+
+        const speed = 20;
+
+        // left button
+        if(this.cursors.left?.isDown) {
+            this.chara.play('charLeft')
+            this.chara.setVelocity(-speed * delta, 0)
+        }
+
+        // right button
+        else if(this.cursors.right?.isDown) {
+            this.chara.play('charRight')
+            this.chara.setVelocity(speed * delta, 0)
+        }
+
+        // up button
+        else if(this.cursors.up?.isDown) {
+            this.chara.play('charBack')
+            this.chara.setVelocity(0, -speed * delta)
+        }
+
+        // down button
+        else if(this.cursors.down?.isDown) {
+            this.chara.play('charFront')
+            this.chara.setVelocity(0, speed * delta)
+        }
+
+        else {
+            this.chara.setVelocity(0, 0)
+        }
     }
 }
